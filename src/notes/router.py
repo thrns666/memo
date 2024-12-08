efd805cbcd7a015862032c68c336d2f4ba082223
thrns666 <super.tawer@gmail.com>
2024-12-08 20:44:16 +0300

    ref, add share link button

--- src/notes/router.py
+++ src/notes/router.py
@@ -1,27 +1,31 @@
-from fastapi import APIRouter, Depends, Form, Cookie
+import base64
+
+from fastapi import APIRouter, Depends, Form, Cookie, HTTPException
 from loguru import logger
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 from starlette.requests import Request
+from starlette.responses import JSONResponse
 from starlette.templating import Jinja2Templates
 
-from notes.models import Note
-from src.postgres_config.database import get_async_session
 from src.auth.utils import get_user_from_token
+from src.notes.models import Note
 from src.notes.schemas import NoteDataForm, NoteData
+from src.notes.utils import create_share_link
 from src.postgres_config.dao import NoteDAO
+from src.postgres_config.database import get_async_session
 
 memo_router = APIRouter()
 templates = Jinja2Templates(directory='../static/templates')
 
 
 @memo_router.get('/')
-async def get_home(request: Request, user: str = Cookie(default=None)):
+async def get_home(request: Request, auth_token: str = Cookie(default=None)):
     return templates.TemplateResponse(
         request=request,
         name='home_page.html',
         status_code=status.HTTP_200_OK,
-        context={'user': user}
+        context={'user': auth_token}
     )
 
 
@@ -146,3 +150,63 @@ async def get_note_by_id(
             context={'detail': ex, 'user': user.get('email')},
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
+
+
+@memo_router.get('/{share_note}')
+async def get_shared_note(
+        request: Request,
+        share_note: str,
+        session: AsyncSession = Depends(get_async_session),
+        auth_token: str = Cookie(default=None)
+):
+    try:
+        data = base64.b64decode(share_note)
+        data = data.decode('UTF-8')
+        owner_email, note_id = data.split('+')
+
+        res = await NoteDAO.get_one_or_none(session=session, owner_email=owner_email, id=int(note_id))
+
+        return templates.TemplateResponse(
+            request=request,
+            name='view_note_page.html',
+            context={'note': res, 'user': auth_token},
+            status_code=status.HTTP_200_OK
+        )
+    except Exception as ex:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=ex)
+
+
+@memo_router.get('/notes/{note_id}/create_share_link_for_note')
+async def get_create_share_link(
+        note_id: int,
+        user: dict = Depends(get_user_from_token),
+        session: AsyncSession = Depends(get_async_session)
+):
+    try:
+        res: Note = await NoteDAO.get_one_or_none(id=note_id, session=session)
+
+        if res and user.get('email') != res.owner_email:
+            return HTTPException(
+                detail='This user dont have notes with this id',
+                status_code=status.HTTP_403_FORBIDDEN
+            )
+        elif not res:
+            return HTTPException(
+                detail='Note with this id does not exist',
+                status_code=status.HTTP_404_NOT_FOUND
+            )
+        elif res:
+            link_json = await create_share_link(note=res, url='127.0.0.1:8000')
+
+            return JSONResponse(
+                content=link_json,
+                status_code=status.HTTP_200_OK,
+                media_type='application/json'
+            )
+
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Try again')
+
+    except Exception as ex:
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'{ex}')
+
+# Need: - realize editing user notes, change encode from base64 → JWT with expire date
--- src/postgres_config/database.py
+++ src/postgres_config/database.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
@@ -16,3 +18,18 @@ async_session = async_sessionmaker(engine, expire_on_commit=False)
 async def get_async_session() -> AsyncSession:
     async with async_session() as session:
         yield session
+
+
+async def cron_del():
+    """
+    Trigger for interval deletion rows in NOTE_SHARE_LINK_TABLE
+
+    :return: NoReturn
+    """
+    with engine.connect() as conn:
+        # Выполняем создание функции и триггера
+        await conn.execute(text(
+            ''' CREATE OR REPLACE FUNCTION delete_old_records() RETURNS TRIGGER AS $$ BEGIN DELETE FROM example WHERE created_at < NOW() - INTERVAL '10 minutes'; RETURN NULL; END; $$ LANGUAGE plpgsql; '''))
+
+        await conn.execute(text(
+            ''' CREATE TRIGGER delete_old_records_trigger AFTER INSERT ON example FOR EACH STATEMENT EXECUTE PROCEDURE delete_old_records(); '''))
